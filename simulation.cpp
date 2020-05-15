5b790534e5833466fa2ca0750ed5049f24412d34
nicmontalti <nicmontalti@gmail.com>
2020-05-15 20:01:38 +0200

    modified check_position and check_group_position

--- simulation.cpp
+++ simulation.cpp
@@ -5,12 +5,12 @@
 
 bool Simulation::check_everyone_position()
 {
-  auto check_position = [=](Person const& p) {
-    return (p.position.x < 0 || p.position.x > size_ || p.position.y < 0 ||
-            p.position.y > size_);
+  auto const check_position = [=](Person const& p) {
+    return (p.position.x >= 0 && p.position.x <= size_ && p.position.y >= 0 &&
+            p.position.y <= size_);
   };
-  auto check_group_position = [&](People const& group) {
-    auto it = std::find_if(group.begin(), group.end(), check_position);
+  auto const check_group_position = [&](People const& group) {
+    auto it = std::find_if_not(group.begin(), group.end(), check_position);
     return (it == group.end());
   };
   return check_group_position(sir_groups_.S) &&
@@ -35,14 +35,12 @@ Simulation::Simulation(
 {
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::uniform_real_distribution<double> uniform_distribution{
+  std::uniform_real_distribution<double> position_distribution{
       0., static_cast<double>(size_)};
   auto new_starting_state = [&]() {
-    double const x = uniform_distribution(gen);
-    double const y = uniform_distribution(gen);
-    double const vx = uniform_distribution(gen);
-    double const vy = uniform_distribution(gen);
-    return Person{Position{x, y}, Velocity{vx, vy}};
+    double const x = position_distribution(gen);
+    double const y = position_distribution(gen);
+    return Person{Position{x, y}, Velocity{0., 0.}};
   };
   std::generate(sir_groups_.S.begin(), sir_groups_.S.end(), new_starting_state);
   std::generate(sir_groups_.I.begin(), sir_groups_.I.end(), new_starting_state);
