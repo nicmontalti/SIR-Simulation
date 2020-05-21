2993e261fbf4929be1107a71302cb47f71d26496
nicmontalti <nicmontalti@gmail.com>
2020-05-21 22:55:53 +0200

    added sane_to_infected, infected_to_sane, update added Simple_Infection constructor removed protected members from G_Infection

--- infection.cpp
+++ infection.cpp
@@ -1,1 +1,66 @@
 #include "infection.hpp"
+#include <cassert>
+#include <cmath>
+
+Simple_Infection::Simple_Infection(int const ticks_frequency,
+                                   double limiting_distance,
+                                   float infection_probability,
+                                   float recovery_probability)
+    : ticks_frequency_{ticks_frequency}
+    , limiting_distance_{limiting_distance}
+    , infection_probability_{infection_probability}
+    , recovery_probability_{recovery_probability}
+    , random_seed_{std::random_device{}()}
+    , probability_distribution_{0.F, 1.F}
+{
+  assert(ticks_frequency_ > 0);
+  assert(limiting_distance_ > 0);
+  assert(infection_probability_ >= 0 && infection_probability_ <= 1);
+  assert(recovery_probability_ >= 0 && recovery_probability_ <= 1);
+}
+
+double Simple_Infection::distance(Person const& left, Person const& right)
+{
+  double const x_distance = left.position.x - right.position.x;
+  double const y_distance = left.position.y - right.position.y;
+  return std::sqrt(x_distance * x_distance + y_distance * y_distance);
+}
+
+void Simple_Infection::sane_to_infected(SIR_Population& population)
+{
+  auto last_sane = population.S.end();
+  auto last_infected = population.I.end();
+  // using auto& it_sane
+  // error: cannot bind non-const lvalue reference of type
+  // ‘__gnu_cxx::__normal_iterator<Person*, std::vector<Person> >&’ to an
+  // rvalue of type ‘std::vector<Person>::iterator {aka
+  // __gnu_cxx::__normal_iterator<Person*, std::vector<Person> >}’
+  for (auto it_sane = population.S.begin(); it_sane != last_sane; ++it_sane) {
+    for (auto it_infected = population.I.begin(); it_infected != last_infected;
+         ++it_infected) {
+      if (distance(*it_sane, *it_infected) < limiting_distance_) {
+        if (probability_distribution_(random_seed_) < infection_probability_) {
+          std::iter_swap(it_sane, last_sane);
+          population.I.push_back(*last_sane);
+          population.S.erase(last_sane);
+          last_sane = population.S.end();
+          last_infected = population.I.end();
+        }
+      }
+    }
+  }
+}
+
+void Simple_Infection::infected_to_recovered(SIR_Population& population)
+{
+  auto last_infected = population.I.end();
+  for (auto it_infected = population.I.begin(); it_infected != last_infected;
+       ++it_infected) {
+    if (probability_distribution_(random_seed_) < recovery_probability_) {
+      std::iter_swap(it_infected, last_infected);
+      population.R.push_back(*last_infected);
+      population.I.erase(last_infected);
+      last_infected = population.I.end();
+    }
+  }
+}
--- infection.hpp
+++ infection.hpp
@@ -1,26 +1,44 @@
 #ifndef INFECTION_HPP
 #define INFECTION_HPP
 
+#include <random>
 #include "SIR_population.hpp"
 
 class G_Infection
 {
- protected:
-  int const size_;
-  SIR_Population& population_;
-  unsigned int const& ticks_;
-
  public:
-  virtual void update()
-  {
-  }
+  bool virtual update(SIR_Population&, unsigned int const& ticks = 0) = 0;
 };
 
 class Simple_Infection : public G_Infection
 {
+  int const ticks_frequency_;
+  double const limiting_distance_;
+  float const infection_probability_;
+  float const recovery_probability_;
+
+  std::mt19937 random_seed_;
+  std::uniform_real_distribution<float> probability_distribution_;
+
+  double distance(Person const&, Person const&);
+  void sane_to_infected(SIR_Population&);
+  void infected_to_recovered(SIR_Population&);
+
  public:
-  void update()
+  Simple_Infection(int const ticks_frequency,
+                   double limiting_distance,
+                   float infection_probability,
+                   float recovery_probability);
+
+  bool update(SIR_Population& population, unsigned int const& ticks) override
   {
+    if (ticks % ticks_frequency_ == 0) {
+      sane_to_infected(population);
+      infected_to_recovered(population);
+      return true;
+    } else {
+      return false;
+    }
   }
 };
 
