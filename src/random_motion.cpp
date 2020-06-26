b7672d53af0f1a9694cb6a9e4732d93ed2addc00
nicmontalti <nicmontalti@gmail.com>
2020-06-26 13:15:10 +0200

    eliiminated friction

--- src/random_motion.cpp
+++ src/random_motion.cpp
@@ -4,31 +4,25 @@
 using namespace sir;
 
 void Random_Motion::update(Population& population, int size)
-  {
-    auto move = [&](Person& person) {
-      person.velocity.vx += random_generator_.Gaus(0., sd_);
-      person.velocity.vy += random_generator_.Gaus(0., sd_);
+{
+  auto move = [&](Person& person) {
+    person.velocity.vx += random_generator_.Gaus(0., sd_);
+    person.velocity.vy += random_generator_.Gaus(0., sd_);
 
-      // Friction
-      person.velocity.vx -=
-          std::abs(person.velocity.vx) * person.velocity.vx / 10000;
-      person.velocity.vy -=
-          std::abs(person.velocity.vy) * person.velocity.vy / 10000;
+    person.position.x += person.velocity.vx;
+    person.position.y += person.velocity.vy;
 
+    if (person.position.x > size || person.position.x < 0) {
+      person.velocity.vx = -person.velocity.vx;
       person.position.x += person.velocity.vx;
+    }
+    if (person.position.y > size || person.position.y < 0) {
+      person.velocity.vy = -person.velocity.vy;
       person.position.y += person.velocity.vy;
+    }
+  };
 
-      if (person.position.x > size || person.position.x < 0) {
-        person.velocity.vx = -person.velocity.vx;
-        person.position.x += person.velocity.vx;
-      }
-      if (person.position.y > size || person.position.y < 0) {
-        person.velocity.vy = -person.velocity.vy;
-        person.position.y += person.velocity.vy;
-      }
-    };
-
-    std::for_each(population.S.begin(), population.S.end(), move);
-    std::for_each(population.I.begin(), population.I.end(), move);
-    std::for_each(population.R.begin(), population.R.end(), move);
-  }
+  std::for_each(population.S.begin(), population.S.end(), move);
+  std::for_each(population.I.begin(), population.I.end(), move);
+  std::for_each(population.R.begin(), population.R.end(), move);
+}
